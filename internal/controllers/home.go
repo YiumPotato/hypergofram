5aa2f00735786ba28d5d084b61e736ab792e6437
Joe Tannoury <joetann20@gmail.com>
2024-01-03 17:13:14 +0100

    Fetch todos before rendering Home function

--- internal/controllers/home.go
+++ internal/controllers/home.go
@@ -3,12 +3,23 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/YiumPotato/hypergofram/internal/db"
 	"github.com/YiumPotato/hypergofram/internal/templates"
 	"github.com/gin-gonic/gin"
 )
 
 func Home(c *gin.Context) {
-	c.HTML(http.StatusOK, "", templates.Home())
+	todos, err := db.QueryMaker.ListTodos(c.Request.Context(), db.ListTodosParams{
+		Limit:  100,
+		Offset: 0,
+	})
+
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "", templates.Error(err.Error()))
+		return
+	}
+
+	c.HTML(http.StatusOK, "", templates.Home(todos))
 }
 
 func About(c *gin.Context) {
